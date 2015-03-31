8513f8419fdd0c2039babbd7fbc3838350dc41f2
raulvivan <raul.vivanco@opendeusto.es>
2015-03-31 14:58:19 +0200

    Implementado metodo GetPilasTapadas, test funciona

--- src/solitario/StartController.java
+++ src/solitario/StartController.java
@@ -29,8 +29,25 @@ public class StartController {
 	}
 
 	public ArrayList<ArrayList<Card>> getPilasTapadas() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<ArrayList<Card>> pilas = new ArrayList<ArrayList<Card>>();
+		for(int i = 0; i<7 ; i++){
+			int numCartas = 0;
+			Card card;
+			ArrayList<Card> cartas = new ArrayList<Card>();
+			for(int j = 0; j<numCartas; j++){
+				do{
+					Random r = new Random();
+					card = new Card(r.nextInt(3), r.nextInt(11), false);
+					
+				}while(cartas.contains(card));
+				{
+					cartas.add(card);
+				}
+			}
+			numCartas++;
+			pilas.add(cartas);
+		}
+		return pilas;
 	}
 
 }
